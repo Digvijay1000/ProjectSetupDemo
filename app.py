e33e2efb09399965201570c29d17765ba125ded0
analyticlimitation <dlsmohite@gmail.com>
2024-02-16 18:53:38 +0530

    data ingestion completed

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 import sys
-
+from src.mlproject.components.data_ingestion import DataIngestion, DataIngestionConfig
 
 if __name__ == "__main__":
-    logging.INFO('The execution has started')
+    logging.info('The execution has started')
 
     try:
-        a=1/0
+        # data_ingestion_config = DataIngestionConfig
+        data_ingestion = DataIngestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
 
     except Exception as e:
         logging.info("Custom Exception")
